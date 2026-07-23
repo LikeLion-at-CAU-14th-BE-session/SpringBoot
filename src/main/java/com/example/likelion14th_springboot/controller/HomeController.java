8b0fca0168e8f757ec4d578c66eb4b86b07e039a
YoonisKim <runheng1209@gmail.com>
2026-07-24 01:31:43 +0900

    feat: 세션 자료 완성

---
+++ src/main/java/com/example/likelion14th_springboot/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.likelion14th_springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the homepage!";
+    }
+}
